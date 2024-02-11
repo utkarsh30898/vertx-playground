f91bfd54b7112eb1c80c341e431b35183f34d21b
utkarsh30898 <utkarsh30898@gmail.com>
2024-02-11 16:48:52 +0530

    feat(vertx): Introduction to Router

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -2,13 +2,22 @@ package com.example.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response().end("Hello World from Vert.x");
-        }).listen(8080);
+        Router router = Router.router(vertx);
+        router.get("/api/v1/hello").handler(ctx -> {
+            ctx.request().response().end("Hello World form vert.x");
+        });
+        router.get("/api/v1/hello/:name").handler(ctx -> {
+            String name = ctx.pathParam("name");
+            ctx.request().response()
+                    .end(String.format("Hello vert.x world from %s", name));
+        });
+
+        vertx.createHttpServer().requestHandler(router).listen(8080);
     }
 }

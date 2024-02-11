64a4a2652c51770d7ece54e4f4b3ebc390a4ded1
utkarsh30898 <utkarsh30898@gmail.com>
2024-02-11 19:43:22 +0530

    feat(vertx): Modular Code

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,23 +1,25 @@
 package com.example.starter;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
         Router router = Router.router(vertx);
-        router.get("/api/v1/hello").handler(ctx -> {
-            ctx.request().response().end("Hello World form vert.x");
-        });
-        router.get("/api/v1/hello/:name").handler(ctx -> {
-            String name = ctx.pathParam("name");
-            ctx.request().response()
-                    .end(String.format("Hello vert.x world from %s", name));
-        });
+        router.get("/api/v1/hello").handler(this::helloVertx);
+        router.get("/api/v1/hello/:name").handler(this::helloNameVertx);
 
         vertx.createHttpServer().requestHandler(router).listen(8080);
+    }
+
+    public void helloVertx (RoutingContext context) {
+        context.request().response().end("Hello World form vert.x");
+    }
+    public void helloNameVertx (RoutingContext context) {
+        String name = context.pathParam("name");
+        context.request().response().end(String.format("Hello vert.x world from %s", name));
     }
 }

d3f62e8416d837597c208cb815cfe7d6b85c978b
utkarsh30898 <utkarsh30898@gmail.com>
2024-02-15 23:59:45 +0530

    feat(vertx): understanding composition and Futures

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -5,6 +5,7 @@ import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.*;
 
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -76,6 +77,30 @@ public class MainVerticle extends AbstractVerticle {
 //            } catch (Exception e) {
 //                promise.fail(e);
 //            }
+//    }
+
+//    Future<HttpServer> startHttpServer(Router router) {
+//        JsonObject http = config().getJsonObject("http");
+//        int port = http.getInteger("port");
+////        vertx.createHttpServer().requestHandler(router).listen(port);
+//        HttpServer server = vertx.createHttpServer().requestHandler(router);
+//
+//        //when we go to server actually listening we're passing in port & promise that will get resolved
+//        // as the async handler...So when server is succesfully up & listening it'll successfully resolve the
+//        //promise and we are returning the result promise wrapped in a future.
+//        return Future.<HttpServer>future(promise -> server.listen(port, promise));
+//    }
+
+
+//    Future<Void> deployOtherVerticles(HttpServer server) {
+//        Future<String> helloA = Future.future(promise -> vertx.deployVerticle("Hello.A", promise));
+//        Future<String> helloB = Future.future(promise -> vertx.deployVerticle("Hello.B", promise));
+//
+//        // have two futures and wanna make sure that both of them are done before we move on two the next step
+//        // use CompositeFuture will help you to resolve any/all of them
+//
+//        return CompositeFuture.all(helloA, helloB).mapEmpty();
+            // it is gonna deploy them in parallel
 //    }
 
     public void helloVertx (RoutingContext context) {
